9c5c6b6c05e68698eb0ca70e2ef40f01f99835a1
daniel96d <78188106+daniel96d@users.noreply.github.com>
2022-04-27 13:30:51 -0700

    make it better

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,9 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/db-migrate', function(){
+  Artisan::call('migrate');
+  echo Artisan::output();
+});
+
 require __DIR__.'/auth.php';
